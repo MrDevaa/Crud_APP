259a054056042482bd8aae1a9bc4f1561261ab8f
Devanandhan Codandabany <d.codandabany@gmail.com>
2024-03-18 21:58:26 +0100

    create a database dans mysql et get in the console

--- crudapp/src/components/CreateUser.js
+++ crudapp/src/components/CreateUser.js
@@ -1,5 +1,66 @@
-export default function CreateUser () {
+import { useState } from "react";
+
+export default function ListUser () {
+
+    const [inputs, setInputs] = useState({})
+
+    const handleChange = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value }));
+    }
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+
+        console.log(inputs);
+    }
+
     return (
-        <h1>Create User :</h1>
+        <div>
+        <h1>List User</h1>
+        <form onSubmit={handleSubmit}>
+            <table cellSpacing="10">
+                    <tbody>
+                        
+                        <tr>
+                            <th>  
+                                <label>Name :</label>
+                            </th>
+                            <td>
+                            <input type="texte" name="name" onChange={handleChange} />
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <th>
+                                 <label>Email :</label>
+                            </th>
+                            <td>
+                            <input type="texte" name="email" onChange={handleChange}/>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <th>
+                            <label>Mobile :</label>
+                            </th>
+                            <td>
+                            <input type="texte" name="mobile" onChange={handleChange}/>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td colSpan="2" align="right">
+                            <button>Save</button>
+                            </td>
+                        </tr>
+                  
+                    </tbody>
+            </table>
+
+        </form>
+    </div>
+       
     )
 }
--- crudapp/src/components/ListUser.js
+++ crudapp/src/components/ListUser.js
@@ -1,5 +1,5 @@
 export default function ListUser () {
     return (
-        <h1>List User :</h1>
+        <h1>List user :</h1>
     )
 }
