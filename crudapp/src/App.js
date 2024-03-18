806d49c32b92097053a43ae0ff4bb2f82d14ec73
Devanandhan Codandabany <d.codandabany@gmail.com>
2024-03-18 01:06:30 +0100

    fist commit but fixed routing

---
+++ crudapp/src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import ListUser from './components/ListUser';
+import CreateUser from './components/CreateUser';
+import EditUser from './components/EditUser';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h5> React Crud Operation :</h5>
+
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/"> List User </Link>
+            </li>
+            <li>
+              <Link to="user/create"> Create User</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route index element={<ListUser />} />
+          <Route path="user/create" element={<CreateUser />} />
+          <Route path="user/:id/edit" element={<EditUser />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ readme.md
@@ -0,0 +1,6 @@
+Crud App React, avec PHP, API, MySQL database.
+- installler node, npm
+- create react app
+- create links et routing dans react
+- create database en MySQL
+- creat GET, POST, PUT, et DELETE API en PHP
